9cf6acef0865e0c782704d3f872a3d687a6d298f
Vinicius Daminelli <78765622+vinidaminelli@users.noreply.github.com>
2022-11-03 21:10:46 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner () {
+
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/Banner.png" alt="Banner principal Organo"></img>
+        </header>
+    )
+}
+
+export default Banner;
